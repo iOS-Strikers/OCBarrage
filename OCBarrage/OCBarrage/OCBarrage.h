57ab44d807a7a65026a1bc82787676300c84b6a8
lifeichao <lifeichao@qmtv.com>
2017-08-26 00:18:05 +0800

    修改文件结构添加pod支持

---
+++ OCBarrage/OCBarrage/OCBarrage.h
@@ -0,0 +1,18 @@
+//
+//  OCBarrage.h
+//  OCBarrage
+//
+//  Created by QMTV on 2017/8/25.
+//  Copyright © 2017年 LFC. All rights reserved.
+//
+
+#ifndef OCBarrage_h
+#define OCBarrage_h
+
+#import "OCBarrageHeader.h"
+#import "OCBarrageManager.h"
+#import "OCBarrageRenderView.h"
+#import "OCBarrageDescriptor.h"
+#import "OCBarrageCell.h"
+
+#endif /* OCBarrage_h */
